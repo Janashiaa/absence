d2f6cc69c6f495e37a33cda9a9fa3280d85512dd
janashiaa <davitjanashia3@gmail.com>
2024-05-11 03:07:39 +0400

    full responsive added

--- frontScript.js
+++ frontScript.js
@@ -1,3 +1,7 @@
 function vacationClicked() {
     window.location.href = "vacationDays/vacation.html"
 }
+
+function sickClicked() {
+    window.alert("Working on it. I could not determine the exact way of calculating sick days. I will suggest this option in the closest future.")
+}
